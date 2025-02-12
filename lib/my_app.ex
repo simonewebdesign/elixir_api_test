0ec32e25976e027e7b8ddfb702091c244268a385
Simone <hello@simonewebdesign.it>
2025-02-12 15:58:56 +0000

    Add implementation and tests

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -15,4 +15,32 @@ defmodule MyApp do
   def hello do
     :world
   end
+
+  # function get_owned_vehicles(person_ids: List<String>): List<OwnedVehicle>
+  # “Real” third party source behind this function
+  def get_owned_vehicles(_person_ids) do
+    [
+      %{person_id: "P1", vehicle_id: "V3"},
+      %{person_id: "P1", vehicle_id: "V8"},
+      %{person_id: "P2", vehicle_id: "V6"}
+    ]
+  end
+
+  # function find_potential_upsells(policies: List<Policy>): List<UpsellOpportunity>
+  def find_potential_upsells(policies) do
+    person_ids = Enum.map(policies, fn policy -> policy[:person_id] end)
+
+    person_ids
+    |> get_owned_vehicles()
+    |> Enum.filter(fn owned_vehicle ->
+      Enum.any?(policies, fn p ->
+        p[:person_id] == owned_vehicle[:person_id] and
+          p[:vehicle_id] != owned_vehicle[:vehicle_id]
+      end)
+    end)
+  end
 end
+
+# person_ids
+# |> get_owned_vehicles
+# |> find_potential_upsells
--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -5,4 +5,25 @@ defmodule MyAppTest do
   test "greets the world" do
     assert MyApp.hello() == :world
   end
+
+  test "get_owned_vehicles returns the vehicles" do
+    assert MyApp.get_owned_vehicles(["P1", "P2"]) == [
+             %{person_id: "P1", vehicle_id: "V3"},
+             %{person_id: "P1", vehicle_id: "V8"},
+             %{person_id: "P2", vehicle_id: "V6"}
+           ]
+  end
+
+  test "returns the expected response" do
+    policies = [
+      %{person_id: "P1", vehicle_id: "V8"},
+      %{person_id: "P2", vehicle_id: "V6"}
+    ]
+
+    expected = [
+      %{person_id: "P1", vehicle_id: "V3"}
+    ]
+
+    assert MyApp.find_potential_upsells(policies) == expected
+  end
 end
