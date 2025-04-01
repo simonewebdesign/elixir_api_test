6839c2532fd39e346337f6f6d1386c5379696b45
Simone <hello@simonewebdesign.it>
2025-04-01 22:42:37 +0100

    Remove old tests

--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -5,44 +5,4 @@ defmodule MyAppTest do
   test "greets the world" do
     assert MyApp.hello() == :world
   end
-
-  test "get_owned_vehicles returns the vehicles" do
-    assert MyApp.get_owned_vehicles(["P1", "P2"]) == [
-             %{person_id: "P1", vehicle_id: "V3"},
-             %{person_id: "P1", vehicle_id: "V8"},
-             %{person_id: "P2", vehicle_id: "V6"}
-           ]
-
-    assert MyApp.get_owned_vehicles(["P2", "P3"]) == [
-             %{person_id: "P2", vehicle_id: "V6"},
-             %{person_id: "P3", vehicle_id: "V9"},
-             %{person_id: "P3", vehicle_id: "V5"},
-             %{person_id: "P3", vehicle_id: "V2"}
-           ]
-  end
-
-  test "returns the expected response" do
-    policies = [
-      %{person_id: "P1", vehicle_id: "V8"},
-      %{person_id: "P2", vehicle_id: "V6"}
-    ]
-
-    expected = [
-      %{person_id: "P1", vehicle_id: "V3"}
-    ]
-
-    assert MyApp.find_potential_upsells(policies) == expected
-
-    policies = [
-      %{person_id: "P3", vehicle_id: "V2"},
-      %{person_id: "P2", vehicle_id: "V6"}
-    ]
-
-    expected = [
-      %{person_id: "P3", vehicle_id: "V9"},
-      %{person_id: "P3", vehicle_id: "V5"}
-    ]
-
-    assert MyApp.find_potential_upsells(policies) == expected
-  end
 end
